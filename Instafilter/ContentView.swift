de7c610e605fcfd523621f46f3c9cc41d2f1f1e9
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-02 11:27:35 +0100

    Building the Basic UI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -2,21 +2,54 @@
 //  ContentView.swift
 //  Instafilter
 //
-//  Created by Buhecha, Neeta (Trainee Engineer) on 02/09/2024.
+//  Created by Buhecha, Neeta on 02/09/2024.
+//
+//  Building the Basic UI
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack{
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack{
+                    Button("Change Filter", action: ChangeFilter)
+                    
+                    Spacer()
+                    // share the picture
+                }
+                
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .padding()
     }
+    
+    func ChangeFilter() {
+        // function here
+    }
+
 }
 
 #Preview {
